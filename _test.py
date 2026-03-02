72a78f9b05f148d8139525425be43a9159eb0b7f
ANA LETICIA DE ARAUJO <129103139+analeticiaraujo@users.noreply.github.com>
2026-03-02 10:02:24 -0300

    refatoração para simplificar os testes utilizando escrevendo uma única vez a c = Calculadora()

--- _test.py
+++ _test.py
@@ -1,25 +1,21 @@
 from calculadora import Calculadora
 
+c = Calculadora()
+
 def test_soma():
-    calc = Calculadora()
-    assert calc.soma(3, 4) == 7
+    assert c.soma(3, 4) == 7
 
 def test_subtracao():
-    calc = Calculadora()
-    assert calc.subtracao(3, 4) == -1
+    assert c.subtracao(3, 4) == -1
 
 def test_multiplicacao():
-    calc = Calculadora()
-    assert calc.multiplicacao(3, 4) == 12
+    assert c.multiplicacao(3, 4) == 12
 
 def test_divisao():
-    calc = Calculadora()
-    assert calc.divisao(8, 4) == 2
+    assert c.divisao(8, 4) == 2
 
 def test_exponencial():
-    calc = Calculadora()
-    assert calc.exponencial(4, 2) == 16
+    assert c.exponencial(4, 2) == 16
 
 def test_divisaoresto():
-    calc = Calculadora()
-    assert calc.divisaoresto(4, 3) == 1
+    assert c.divisaoresto(4, 3) == 1
